4b8024ad8ed8c866fabb8804f1e36e46c255de9e
fabulousduck <ryanvlaming@icloud.com>
2017-08-05 20:01:32 +0200

    read info header

--- main.c
+++ main.c
@@ -3,34 +3,66 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct Bitmap {
+typedef struct {
     unsigned short int header_id;
     unsigned int file_size;
     unsigned short int reserved_1; //these differ based upon creation program
     unsigned short int reserved_2;
     unsigned int pixel_offset;
-} Bitmap;
+} BitmapHeader;
+
+typedef struct {
+    unsigned int header_size;
+    unsigned int width;
+    unsigned int height;
+    unsigned short int color_planes;
+    unsigned short int depth;
+    unsigned int compression;
+    unsigned int image_size;
+    unsigned int hor_res;
+    unsigned int ver_res;
+    unsigned int num_colors;
+    unsigned int important_num_colors;
+} InfoHeader;
 
 int main() {
     FILE *fp = fopen("test.bmp", "r");
-    Bitmap bitmap;
+    BitmapHeader bitmapHeader;
+    InfoHeader infoHeader;
 
     //read header id
-    fread(&bitmap.header_id, 2,1,fp);
+    fread(&bitmapHeader.header_id, 2,1,fp);
 
     //set header size
-    fread(&bitmap.file_size, 4,1,fp);
+    fread(&bitmapHeader.file_size, 4,1,fp);
 
     //set reserved1
-    fread(&bitmap.reserved_1, 2, 1, fp);
+    fread(&bitmapHeader.reserved_1, 2, 1, fp);
 
     //set reserved2
-    fread(&bitmap.reserved_2, 2, 1, fp);
+    fread(&bitmapHeader.reserved_2, 2, 1, fp);
 
     //set pixel offset
-    fread(&bitmap.pixel_offset, 4, 1, fp);
+    fread(&bitmapHeader.pixel_offset, 4, 1, fp);
 
-    printf("file size : %d\n reserved_1: %d\n reserved_2: %d\n pixel offset: %d\n", bitmap.file_size, bitmap.reserved_1, bitmap.reserved_2, bitmap.pixel_offset);
+    printf("file size : %d\n reserved_1: %d\n reserved_2: %d\n pixel offset: %d\n", bitmapHeader.file_size, bitmapHeader.reserved_1, bitmapHeader.reserved_2, bitmapHeader.pixel_offset);
+
+    //read the info header into the InfoHeader struct
+    fread(&infoHeader, sizeof(InfoHeader), 1, fp);
+
+
+    printf("header_size: %d\n", infoHeader.header_size);
+    printf("width: %d\n", infoHeader.width);
+    printf("height: %d\n", infoHeader.height);
+    printf("color_planes: %d\n", infoHeader.color_planes);
+    printf("depth: %d\n", infoHeader.depth);
+    printf("compression: %d\n", infoHeader.compression);
+    printf("image_size: %d\n", infoHeader.image_size);
+    printf("hor_res: %d\n", infoHeader.hor_res);
+    printf("ver_res: %d\n", infoHeader.ver_res);
+    printf("num_colors: %d\n", infoHeader.num_colors);
+    printf("important_num_colors: %d\n", infoHeader.important_num_colors);
+
     return 0;
 }
 
